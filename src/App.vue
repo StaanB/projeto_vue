e9c7d8196c07012957d01a5b2bc0863b1a509f50
Stanley <stanleybrenner@gmail.com>
2023-10-09 12:36:33 -0300

    calculadora com Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,58 @@
 <script setup>
-import Cabecalho from "./components/Cabecalho.vue";
-import Formulario from "./components/Form.vue";
-import ListaDeTarefas from "./components/ListaDeTarefas.vue";
-
-
 import { reactive } from 'vue';
 
 const estado = reactive({
-  filtro: "todas",
-  tarefaTemporaria: "",
-  tarefas: [
-    {
-      titulo: 'Estudar ES6',
-      finalizada: false,
-    },
-    {
-      titulo: "Estudar SASS",
-      finalizada: false
-    },
-    {
-      titulo: "Go to gym",
-      finalizada: true
-    }
-  ]
+  numero1: 0,
+  numero2: 0,
+  conta: "somar",
+  resultado: 0,
 })
 
-const cadastraTarefa = () => {
-  const tarefaNova = {
-    titulo: estado.tarefaTemporaria,
-    finalizada: false
+const executarConta = (e) => {
+
+  if (e.target.id === 'num1') {
+    estado.numero1 = parseInt(e.target.value)
+  } else if (e.target.id === "num2") {
+    estado.numero2 = parseInt(e.target.value)
+  } else {
+    estado.conta = e.target.value
   }
-  estado.tarefas.push(tarefaNova)
-  estado.tarefaTemporaria = ''
-}
 
-const getTarefasPendentes = () => {
-  return estado.tarefas.filter(tarefa => !tarefa.finalizada)
-}
+  const { conta } = estado
 
-const getTarefasFinalizadas = () => {
-  return estado.tarefas.filter(tarefa => tarefa.finalizada)
-}
-
-const getTarefasFiltradas = () => {
-  const { filtro } = estado
-
-  switch (filtro) {
-    case 'pendentes':
-      return getTarefasPendentes()
-    case 'finalizadas':
-      return getTarefasFinalizadas()
-    default:
-      return estado.tarefas
+  switch (conta) {
+    case "somar":
+      return estado.resultado = estado.numero1 + estado.numero2
+    case "subtrair":
+      return estado.resultado = estado.numero1 - estado.numero2
+    case "multiplicar":
+      return estado.resultado = estado.numero1 * estado.numero2
+    case "dividir":
+      return estado.resultado = estado.numero1 / estado.numero2
   }
 }
 </script>
 
 <template>
-  <div class="container">
-    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length" />
+  <div class="container bg-dark text-white mt-2 rounded-2" style="height: 30rem;">
+    <div class="d-flex w-50 flex-column container justify-content-center align-items-center">
 
-    <Formulario :tarefa-temporaria="estado.tarefaTemporaria" :edita-tarefa-temp="evento => estado.tarefaTemporaria = evento.target.value" :cadastra-tarefa="cadastraTarefa" :trocar-filtro="evento => estado.filtro = evento.target.value"/>
+      <h1 class="mb-5 mt-5">Calculadora com Vue JS</h1>
 
-    <ListaDeTarefas :tarefas="getTarefasFiltradas()"/>
+      <input @keyup="executarConta" placeholder="Número 1" id="num1" type="number"
+        class="w-25 fs-5 p-2 mb-2 form-control">
 
+      <select @change="executarConta" id="select" class="w-25 fs-5 p-2 mb-2 form-select">
+        <option value="somar">Soma (+)</option>
+        <option value="subtrair">Subtração (-)</option>
+        <option value="multiplicar">Multiplicação (*)</option>
+        <option value="dividir">divisão (/)</option>
+      </select>
 
+      <input @keyup="executarConta" placeholder="Número 2" id="num2" type="number"
+        class="w-25 fs-5 p-2 mb-2 form-control">
+      <p class="fs-2">=</p>
+      <p class="fs-1 fw-bold">{{ estado.resultado }}</p>
+    </div>
   </div>
 </template>
